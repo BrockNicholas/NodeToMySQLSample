bf84f501c4d7e193ef2a4f7c84939001b70f1e6c
brock <bmac@bmacs-MacBook-Pro.local>
2020-01-31 00:07:15 -0500

    Added handler wrapper for Lambda (commented out by default)

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//// = (Uncomment these lines for use in AWS Lambda function)
+
 var mysql = require('mysql');
 var config = require('./config/conn.json');
 
@@ -7,16 +9,22 @@ var pool  = mysql.createPool({
     password : config.dbpassword,
     database : config.dbname
   });
+  // Can use process.env.VAR_NAME instead of config file in AWS Lambda
+  // if you prefer setting environment variables that way. But this way works for
+  // Lambda and other platforms as well
 
-
+//// exports.handler = (event, context, callback) => {
   pool.getConnection(function(err, connection) {
     // Use the connection
+    //// context.callbackWaitsForEmptyEventLoop = false;
     connection.query('SELECT * from test', function (error, results, fields) {
       // And done with the connection.
       connection.release();
       // Handle error after the release.
       if (error) throw error;
       else console.log(results);
+      //// callback(null, results);
       process.exit();
     });
   });
+//// }
