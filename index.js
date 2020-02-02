9710eff8024789c1fe99bd9758a3c87f29f66f22
Brock <bmac@bmacs-MacBook-Pro.local>
2020-02-02 00:04:57 -0500

    Formatting

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ var mysql = require('mysql');
 var config = require('./config/conn.json');
 
 var pool  = mysql.createPool({
-    host     : config.dbhost,
-    user     : config.dbuser,
-    password : config.dbpassword,
-    database : config.dbname
-  });
-  // Can use process.env.VAR_NAME instead of config file in AWS Lambda
-  // if you prefer setting environment variables that way. But this way works for
-  // Lambda and other platforms as well
+  host     : config.dbhost,
+  user     : config.dbuser,
+  password : config.dbpassword,
+  database : config.dbname
+});
+// Can use process.env.VAR_NAME instead of config file in AWS Lambda
+// if you prefer setting environment variables that way. But this way works for
+// Lambda and other platforms as well
 
 //// exports.handler = (event, context, callback) => {
   pool.getConnection(function(err, connection) {
